16f055fdb1e729a3362d66efae85127301ae3abe
rani0227 <ranilekshmi0227@gmail.com>
2025-03-08 12:07:08 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,13 @@
+inputList=[1,2,3,4,2,3,4]
+print(inputList)
+inputList.insert(3,"New element")
+print(inputList)
+inputList.append("Append this")
+print(inputList)
+inputList.remove(2)
+print(inputList)
+inputList.pop()
+print(inputList)
+inputList.pop(3)
+print(inputList)
+    
